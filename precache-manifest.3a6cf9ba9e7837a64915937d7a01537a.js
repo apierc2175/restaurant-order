1250c5be7aa8d8100f99f0281dccac1e361d82ed
apierc2175 <aidanwpierce@gmail.com>
2019-11-18 17:22:36 -0500

    Updates

---
+++ precache-manifest.3a6cf9ba9e7837a64915937d7a01537a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07dc54ee7dd478dc6eda73c17b141f8d",
+    "url": "/restaurant-order/index.html"
+  },
+  {
+    "revision": "c09f47d265aec9dc1b91",
+    "url": "/restaurant-order/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8cc767e640557e787a97",
+    "url": "/restaurant-order/static/css/main.fa4e0d41.chunk.css"
+  },
+  {
+    "revision": "c09f47d265aec9dc1b91",
+    "url": "/restaurant-order/static/js/2.5078f07a.chunk.js"
+  },
+  {
+    "revision": "8cc767e640557e787a97",
+    "url": "/restaurant-order/static/js/main.5a0ec8c1.chunk.js"
+  },
+  {
+    "revision": "72e60424ee167f50aeb7",
+    "url": "/restaurant-order/static/js/runtime-main.87f5c07b.js"
+  }
+]);
